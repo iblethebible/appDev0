b2b5aa73c280959dbebd0d28e5b4352c69bc3416
13079848 <13079848@LC3-SP-10613>
2022-10-06 10:46:20 +0100

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void End()
+        {
+            Console.Write("Press any key to end");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+
+            End();
+        }
+    }
+}
